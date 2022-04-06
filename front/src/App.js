921798e1051ee71133a2103d38c0b873b9bcd9ee
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-06 07:35:41 +0200

    feat: ajout d'un systeme de route

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import Loginpage from './authPages/LoginPage/LoginPage'
+import Registerpage from './authPages/RegisterPage/RegisterPage'
+import Dashboard from './Dashboard/Dashboard'
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+  return <>
+    <Router>
+      <Switch>
+        <Route exact path='/login'>
+          <Loginpage/>
+        </Route>
+        <Route exact path='/register'>
+          <Registerpage/>
+        </Route>
+        <Route exact path='/dashboard'>
+          <Dashboard/>
+        </Route>
+        <Route path='/'>
+          <Redirect to='/dashboard'/>
+        </Route>
+      </Switch>
+    </Router>
+  </>
 }
 
 export default App;
---
+++ front/src/authPages/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registerpage = () => {
+  return (
+    <div>
+      RegiserPage
+    </div>
+  );
+}
+
+export default Registerpage
