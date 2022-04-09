2ee40b3ed2ca967c839df57c05454f975963713e
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-09 08:24:10 +0200

    lien de redirection login vers register

--- front/src/authPages/LoginPage/LoginPageFooter.jsx
+++ front/src/authPages/LoginPage/LoginPageFooter.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton"
+import RedirectInfo from "../../shared/components/RedirectInfo"
+import {useHistory} from 'react-router-dom'
 
 const LoginPageFooter = ({handleLogin, isValid}) => {
+  const history = useHistory()
+
+  const handlePushToRegisterPage = () => {
+    history.push("/register")
+  }
+
   return (
-    <div>
-      <CustomPrimaryButton
-        label="Log in"
-        style={{marginTop: '30px'}}
-        disabled={!isValid}
-        onClick={handleLogin}
+    <>
+      <div>
+        <CustomPrimaryButton
+          label="Log in"
+          style={{marginTop: '30px'}}
+          disabled={!isValid}
+          onClick={handleLogin}
+        />
+      </div>
+      <RedirectInfo
+        text='Need an account? '
+        redirectText='Create an account'
+        style={{marginTop: '5px'}}
+        redirectHandler={handlePushToRegisterPage}
       />
-    </div>
+    </>
+
   );
 };
 
--- front/src/authPages/RegisterPage/RegisterPage.jsx
+++ front/src/authPages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Authbox from "../../shared/components/AuthBox";
 
 const Registerpage = () => {
   return (
-    <div>
+    <Authbox>
       RegiserPage
-    </div>
+    </Authbox>
   );
 }
 
