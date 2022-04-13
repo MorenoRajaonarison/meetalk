59f76ff7de6edcd12ee378b434b5b7aee8b969c6
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-13 18:28:40 +0200

    ajout du jwt dans le requete

--- front/src/api.jsx
+++ front/src/api.jsx
@@ -1,8 +1,20 @@
 import axios from 'axios'
+import {logout} from "./shared/Utils/auth"
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5002/api",
   timeout: 1000
+})
+
+apiClient.interceptors.request.use(config => {
+  const userDetails = localStorage.getItem('user')
+  if(userDetails) {
+    const token = JSON.parse(userDetails).token
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, err => {
+  return Promise.reject(err)
 })
 
 export const login = async (data) => {
@@ -24,5 +36,12 @@ export const register = async (data) => {
       error: true,
       e
     }
+  }
+}
+
+const checkResponseCode = e => {
+  const responseCode = e?.response?.status
+  if(responseCode){
+    (responseCode === 401 || responseCode === 403) && logout()
   }
 }
