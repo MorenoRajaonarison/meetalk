d8298bbae251649fae14896fb4ee035f29b91086
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-14 09:34:15 +0200

    structure du dashboard

--- front/src/Dashboard/Dashboard.jsx
+++ front/src/Dashboard/Dashboard.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
+import {styled} from '@mui/system'
+import Sidebar from "./Sidebar/Sidebar";
+import FriendsSidebar from "./FriendsSidebar/FriendsSidebar";
+import Messenger from "./Messenger/Messenger";
+import Appbar from "./Appbar/Appbar";
+
+const Wrapper = styled('div')({
+  width: '100%',
+  height: '100vh',
+  display: 'flex'
+})
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  );
+  return <Wrapper>
+    <Sidebar/>
+    <FriendsSidebar/>
+    <Messenger/>
+    <Appbar/>
+  </Wrapper>
 }
 
-export default Dashboard;
+export default Dashboard
