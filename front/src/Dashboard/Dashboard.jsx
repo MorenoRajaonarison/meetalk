5ca6c4394b08c35196d2d2f7dc7e421ffc97c034
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-17 11:25:26 +0200

    securisation du dashboard

--- front/src/Dashboard/Dashboard.jsx
+++ front/src/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {styled} from '@mui/system'
-import Sidebar from "./Sidebar/Sidebar";
-import FriendsSidebar from "./FriendsSidebar/FriendsSidebar";
-import Messenger from "./Messenger/Messenger";
-import Appbar from "./Appbar/Appbar";
+import Sidebar from "./Sidebar/Sidebar"
+import FriendsSidebar from "./FriendsSidebar/FriendsSidebar"
+import Messenger from "./Messenger/Messenger"
+import Appbar from "./Appbar/Appbar"
+import {logout} from "../shared/Utils/auth"
+import {connect} from 'react-redux'
+import {getActions} from "../store/actions/authActions"
 
 const Wrapper = styled('div')({
   width: '100%',
@@ -11,7 +14,16 @@ const Wrapper = styled('div')({
   display: 'flex'
 })
 
-const Dashboard = () => {
+const Dashboard = ({setUserDetails}) => {
+  useEffect(() => {
+    const userDetails = localStorage.getItem('user')
+    if(!userDetails) {
+      logout()
+    } else {
+      setUserDetails(JSON.parse(userDetails))
+    }
+  }, []);
+  
   return <Wrapper>
     <Sidebar/>
     <FriendsSidebar/>
@@ -20,4 +32,10 @@ const Dashboard = () => {
   </Wrapper>
 }
 
-export default Dashboard
+const mapActionsToProps = (dispatch) => {
+  return {
+    ...getActions(dispatch)
+  }
+}
+
+export default connect(null, mapActionsToProps)(Dashboard)
