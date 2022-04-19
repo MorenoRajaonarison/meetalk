7db14c2e07d1d1e7386129c9ace65a55ffe718f9
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-19 18:01:31 +0200

    logout handler

--- back/socketServer.js
+++ back/socketServer.js
@@ -1,5 +1,6 @@
 const authSocket = require("./middlewares/authSocket")
 const newConnectionHandler = require('./socketHandlers/newConnectionHandler')
+const disconnectHandler = require("./socketHandlers/disconnectHandler")
 
 const registerSocketServer = server => {
   const io = require('socket.io')(server, {
@@ -17,6 +18,9 @@ const registerSocketServer = server => {
     console.log('user connected')
     console.log(socket.id)
     newConnectionHandler(socket, io)
+    socket.on('disconnect', () => {
+      disconnectHandler(socket)
+    })
   })
 }
 
