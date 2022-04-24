0c1349a73588e6a6850c7f17f9f3900de80fe39b
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-24 11:12:47 +0200

    feat: rejet d'invitation

--- back/controllers/friendInvitation/postReject.js
+++ back/controllers/friendInvitation/postReject.js
@@ -1,5 +1,23 @@
+const FriendInvitation = require('../../models/friendsInvitation')
+const friendsUpdates = require("../../socketHandlers/updates/friends")
+
 const postReject = async (req, res) => {
-  return res.send('reject handler')
+  try {
+    const {id}= req.body
+
+    //suppr l'invitation de la db
+    const invitationExist = await FriendInvitation.exists({_id:id})
+    if(invitationExist){
+      await FriendInvitation.findByIdAndDelete(id)
+    }
+
+    //maj de l'invitation en attente
+    friendsUpdates.updateFriendsPendingInvitation(req.user.id)
+    return res.status(200).send('Invitation rejeter avec success')
+  }catch(e){
+    return res.status(500).send('Une erreur est survenue, Reesayer')
+  }
+
 }
 
 module.exports = postReject
