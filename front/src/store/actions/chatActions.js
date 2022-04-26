36de334bfa8770b66f8ebde36be0afd93606133e
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-04-26 09:50:35 +0200

    preparation du store pour chat

---
+++ front/src/store/actions/chatActions.js
@@ -0,0 +1,31 @@
+export const chatTypes = {
+  DIRECT: 'DIRECT',
+  GROUP: 'GROUP'
+}
+
+export const chatActions = {
+  SET_CHOSEN_CHAT_DETAILS: 'CHAT.SET_CHOSEN_CHAT_DETAILS',
+  SET_MESSAGES: 'CHAT.SET_MESSAGES',
+  SET_CHAT_TYPE: 'CHAR.SET_CHAT_TYPE'
+}
+
+export const getActions = dispatch => {
+  return {
+    setChosenChatDetails: (details, chatType) => dispatch(setChosenChatDetails(details, chatType))
+  }
+}
+
+export const setChosenChatDetails = (chatDetails , chatType) => {
+  return {
+    type: chatActions.SET_CHOSEN_CHAT_DETAILS,
+    chatType,
+    chatDetails
+  }
+}
+
+export const setMessages = messages => {
+  return {
+    type: chatActions.SET_MESSAGES,
+    messages
+  }
+}
