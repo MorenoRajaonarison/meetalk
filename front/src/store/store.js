7b5220ffb2891129efb2b153ea0c549f5164ecce
Moreno Rajaonarison <rajaonarison.moreno@gmail.com>
2022-05-05 07:00:22 +0200

    preparation du store pour l'appel video

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -5,12 +5,14 @@ import authReducer from "./reducers/authReducer"
 import alertReducer from "./reducers/alertReducer"
 import friendsReducer from "./reducers/friendsReducer"
 import chatReducer from './reducers/chatReducer'
+import roomReducer from './reducers/roomReducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   alert: alertReducer,
   friends: friendsReducer,
-  chats: chatReducer
+  chats: chatReducer,
+  room: roomReducer
 })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
